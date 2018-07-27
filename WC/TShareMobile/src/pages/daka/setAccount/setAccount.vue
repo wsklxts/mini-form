1a1a4dce8d0ca664dde4a84274da9c7f2b6b2333
wsklxts <276662340@qq.com>
2018-07-27 09:59:47 +0800

    1

---
+++ WC/TShareMobile/src/pages/daka/setAccount/setAccount.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <XHeader  title="设置" :left-options="{showBack: true}"></XHeader>
+    <div class="template">
+      <section class="btn">
+        <XButton type="default" @click.native="logout()">退出登录</XButton>
+      </section>
+
+      <tabs></tabs>
+    </div>
+
+  </div>
+
+</template>
+
+
+
+<script type="text/ecmascript-6">
+
+  import { Grid, GridItem,XHeader,XButton } from 'vux'
+  import tabs from "../../../components/common/tabs.vue"
+  export default {
+    name:"apply",
+    methods:{
+      logout(){
+        console.log(this.$store);
+        this.$store.commit("changeLogin",1)
+        this.$router.push({path:"/login"})
+      }
+    },
+    data(){
+      return{
+
+      }
+    },
+    components: {
+      Grid,
+      GridItem,
+      XHeader,
+      tabs,
+      XButton,
+    }
+
+
+  }
+</script>
+
+
+<style lang="less" type="text/less" scoped>
+  .btn{
+    padding:0.58rem;
+  }
+
+</style>
